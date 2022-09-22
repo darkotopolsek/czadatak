d1b84b473cba92554d99c09020bf78098e7f8efd
darko topolsek <113949137+darkotopolsek@users.noreply.github.com>
2022-09-22 09:00:44 +0200

    Update DB_schema.sql

--- DB_schema.sql
+++ DB_schema.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE `atest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `user` (
+CREATE TABLE `user2` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `surname` varchar(45) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
+  `dob` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
+  `photo` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
