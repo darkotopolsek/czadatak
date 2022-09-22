7f37eb4184b69fe3d0b037b8cd8d699508cf6e9b
darko topolsek <113949137+darkotopolsek@users.noreply.github.com>
2022-09-22 07:14:10 +0200

    Create DB_schema.sql
    DB and TABLE create statement

---
+++ DB_schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `atest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `surname` varchar(45) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
